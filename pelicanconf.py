3934b726ed97d61fe788739b7847640d2f9601e6
Quinn Neumiiller <quinnjn@gmail.com>
2015-01-30 15:29:34 -0600

    Adding CNAME back

--- pelicanconf.py
+++ pelicanconf.py
@@ -33,5 +33,8 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ['extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'},}
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
